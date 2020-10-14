4bd134bb653e8c7b67ea36416f0bae5fa5147fa0
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-14 15:36:50 -0400

    Product and Rating components

---
+++ frontend/src/components/Product.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import Rating from './Rating';
+
+interface Props {
+    product: {
+        _id: string;
+        name: string;
+        image: string;
+        description: string;
+        brand: string;
+        category: string;
+        price: number;
+        countInStock: number;
+        rating: number;
+        numReviews: number;
+    };
+}
+
+const Product: React.FC<Props> = ({ product }) => {
+    return (
+        <Card className='my-3 p-3 rounded'>
+            <a href={`/product/${product._id}`}>
+                <Card.Img src={product.image} variant='top' />
+            </a>
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as='div'>
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>
+
+                <Card.Text as='div'>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                </Card.Text>
+
+                <Card.Text as='h3'>${product.price}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Product;
