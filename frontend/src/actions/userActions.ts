62d6880b2d7d07c4b67c11906de03d23e4225ef8
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-27 11:03:09 -0400

    Register Screen

--- frontend/src/actions/userActions.ts
+++ frontend/src/actions/userActions.ts
@@ -5,6 +5,9 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
 } from '../constants/userContant';
 
 export const login = (email: string, password: string) => async (dispatch: Dispatch) => {
@@ -39,4 +42,35 @@ export const login = (email: string, password: string) => async (dispatch: Dispa
 export const logout = () => (dispatch: Dispatch) => {
     localStorage.removeItem('userInfo');
     dispatch({ type: USER_LOGOUT });
+};
+
+export const register = (name: string, email: string, password: string) => async (
+    dispatch: Dispatch
+) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST,
+        });
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const { data } = await axios.post('/api/users', { name, email, password, config });
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data,
+        });
+
+        localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (err) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload:
+                err.response && err.response.data.message ? err.response.data.message : err.message,
+        });
+    }
 };
