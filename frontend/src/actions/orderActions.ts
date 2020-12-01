5c1964d9ad52a8c1762df6da67260c28b15f9716
Jacob Bruce <brucejac@sheridancollege.ca>
2020-12-01 09:42:46 -0500

    Order Pay Reducer & Action

--- frontend/src/actions/orderActions.ts
+++ frontend/src/actions/orderActions.ts
@@ -6,6 +6,9 @@ import {
     ORDER_DETAILS_FAIL,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
+    ORDER_PAY_FAIL,
+    ORDER_PAY_REQUEST,
+    ORDER_PAY_SUCCESS,
 } from '../constants/orderConstants';
 import axios from 'axios';
 
@@ -66,6 +69,41 @@ export const getOrderDetails = (id: any) => async (dispatch: Dispatch, getState:
     } catch (err) {
         dispatch({
             type: ORDER_DETAILS_FAIL,
+            payload:
+                err.response && err.response.data.message ? err.response.data.message : err.message,
+        });
+    }
+};
+
+export const payOrder = (orderId: string, paymentResult: any) => async (
+    dispatch: Dispatch,
+    getState: any
+) => {
+    try {
+        dispatch({
+            type: ORDER_PAY_REQUEST,
+        });
+
+        const {
+            userLogin: { userInfo },
+        } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+
+        const { data } = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config);
+
+        dispatch({
+            type: ORDER_PAY_SUCCESS,
+            payload: data,
+        });
+    } catch (err) {
+        dispatch({
+            type: ORDER_PAY_FAIL,
             payload:
                 err.response && err.response.data.message ? err.response.data.message : err.message,
         });
